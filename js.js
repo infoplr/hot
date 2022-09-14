429794bd857d35304ba4ba9a19106df935a2f118
Céo <noah.kd@gmail.com>
2022-09-14 05:26:41 -0300

    BOBBJNNI

--- js.js
+++ js.js
@@ -1,6 +1,4 @@
-// var d19d = require( "./d19d.mjs" );
-// var d19d = await import('./d19d.js');
-// import { d19d } from "d19d.cjs";
+
 
 window.addEventListener( "load", () => {
 
@@ -10,13 +8,14 @@ window.addEventListener( "load", () => {
     var l = document.querySelectorAll( "l" ),
         home = document.querySelector( "#home" ),
         fab = document.querySelector( "fab" ),
+        fabCircle = document.querySelector( "circle" ),
         btn_d19d = document.querySelector( "#btn_d19d" ),
         drawer = document.querySelector( "drawer" ); 
 
     /* [ functions ] 
     =================================*/
 
-    function print( v ) { console.log( v ) }
+    function print( c, e, o ) { o ? console.log( c, e, o ) : !o && e ? console.log( c, e ) : console.log( c ) }
 
 
     /* [ events ] 
@@ -37,6 +36,16 @@ window.addEventListener( "load", () => {
         drawer.style.width == "0px" ?
         drawer.style.width = "clamp( 100px, 70vw, 250px )" :
         drawer.style.width = "0px" ;
+    } );
+    fab.addEventListener( "mouseover", ev => {
+        fab.style.transform = "rotate( 150deg )";
+        fab.style.borderRadius = "24px 15px 24px 15px";
+        fabCircle.style.borderRadius = "24px 15px 24px 15px";
+    } );
+    fab.addEventListener( "mouseleave", ev => {
+        fab.style.transform = "rotate( -5deg )";
+        fab.style.borderRadius = "2rem";
+        fabCircle.style.borderRadius = "2rem";
     } );
 
     btn_d19d.addEventListener( "click", ev => {
